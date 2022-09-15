1d3f29ceb97fbc2c0b668d9c27cd7323fa9f1af0
ANAVADH <anavadhsuresh1042@gmail.com>
2022-09-15 20:28:34 +0530

    sec

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import {store , persistor} from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react';
+import ReactDOM from 'react-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
